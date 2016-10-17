0fb872fc6506c245599a9c8c7aeb3b65ce46dc05
BlackTrub <lightlocdog@gmail.com>
2016-10-17 23:46:27 +0300

    add more files;

---
+++ source/main.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+from bottle import Bottle
+from bottle import run, static_file
+
+from bottle import jinja2_template as template
+
+
+app = Bottle()
+
+
+@app.route('/')
+def index():
+    context = {
+        'name': u'Петрович'
+    }
+    return template('template/base.html', context)
+
+
+@app.error(404)
+def error404(error):
+    return template('template/404.html')
+
+
+@app.get('/<filename:re:.*\.js>')
+def static_js(filename):
+    return static_file(filename, root='static/js')
+
+
+@app.get('/<filename:re:.*\.css>')
+def static_css(filename):
+    return static_file(filename, root='static/css')
+
+
+if __name__ == '__main__':
+    run(app, host='localhost', port=8080, debug=True)
